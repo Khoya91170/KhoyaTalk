42b92bb19766325b621859275e36bbd9f8b9a3fa
Khoya91170 <fouzikheloufi@gmail.com>
2015-11-15 13:56:09 +0100

    Modification folder

--- index.js
+++ index.js
@@ -5,66 +5,53 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var antiSpam = require('socket-anti-spam');
-var clients;
+//var antiSpam = require('socket-anti-spam');
+//var clients;
 
 app.use('/public', express.static(__dirname + '/public')); // It will find static files like css, js files
 //app.use(express.static(__dirname + '/public'));
 
-// These two lines are required to retrieve data from POST method
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({extended: false});
-
-var aUserName =""; // The username will be used in home.html (the chat)
-
-app.get('/', function(req, res){ // function fire on load of the page (127.0.0.1:3000)
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.post('/index', urlencodedParser, function(req, res){  // function fire when post data is send to the uri 127.0.0.1:3000/index
-    //console.log("jrentre ici");
-    //console.log(req.body);
-    aUserName = req.body.username;
-    /*if(aUserName)
-        //console.log("Un username catché !!!");*/
-    if(aUserName && aUserName != '')
-        res.sendFile(__dirname + '/home.html');
-    else
-        res.sendFile(__dirname + '/index.html');
-});
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+})
 
 // Prevent spam from user
-var antiSpam = new antiSpam({
-    spamCheckInterval: 3000,
-    spamMinusPointsPerInterval: 3,
-    spamMaxPointsBeforeKick: 9,
-    spamEnableTempBan: true,
-    spamKicksBeforeTempBan: 3,
-    spamTempBanInMinutes: 10,
-    removeKickCountAfter: 1,
-    debug: false
-});
+/*
+ var antiSpam = new antiSpam({
+ spamCheckInterval: 3000,
+ spamMinusPointsPerInterval: 3,
+ spamMaxPointsBeforeKick: 9,
+ spamEnableTempBan: true,
+ spamKicksBeforeTempBan: 3,
+ spamTempBanInMinutes: 10,
+ removeKickCountAfter: 1,
+ debug: false
+ });*/
 
 
 // When client is connect to the chat (so on the socket) :
-io.on('connection',function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
     console.log('socket = ');
     console.log(socket.id);
-    antiSpam.onConnect(socket);
 
-    socket.on('disconnect', function(){
+    socket.username = 'Unknown';
+
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 
-    socket.on('chat message', function(msg){
-        if(msg != '')
-        {
+    socket.on('chat username', function (data) {
+        socket.username = data;
+    });
+
+    socket.on('chat message', function (msg) {
+        if (msg != '') {
             io.emit('chat message', aUserName + " : " + msg);
         }
     });
 });
 
-http.listen(3000, function(){
-   console.log('listening on 3000') ;
+http.listen(3025, function () {
+    console.log('listening on 3000');
 });
