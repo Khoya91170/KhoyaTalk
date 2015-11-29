11d50edf9821e157a1ff9a910c7cab00b0e29e16
Khoya91170 <fouzikheloufi@gmail.com>
2015-11-29 10:02:40 +0100

    Management of the number of users

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-/**
- * Created by fouzi on 07/11/2015.
- */
+/* Created by fouzi on 07/11/2015. */
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var trim = require('trim');
+var striptags = require("striptags");
+var numberOfConnectedUser = 0;
 
-//var antiSpam = require('socket-anti-spam');
-//var clients;
 
 app.use('/public', express.static(__dirname + '/public')); // It will find static files like css, js files
 //app.use(express.static(__dirname + '/public'));
@@ -16,19 +14,6 @@ app.use('/public', express.static(__dirname + '/public')); // It will find stati
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 })
-
-// Prevent spam from user
-/*
- var antiSpam = new antiSpam({
- spamCheckInterval: 3000,
- spamMinusPointsPerInterval: 3,
- spamMaxPointsBeforeKick: 9,
- spamEnableTempBan: true,
- spamKicksBeforeTempBan: 3,
- spamTempBanInMinutes: 10,
- removeKickCountAfter: 1,
- debug: false
- });*/
 
 
 // When client is connected to the chat (so on the socket) :
@@ -40,11 +25,14 @@ io.on('connection', function (socket) {
     socket.username = 'Unknown';
 
     socket.on('disconnect', function () {
+        if(numberOfConnectedUser > 0)
+            numberOfConnectedUser--;
         console.log('user disconnected');
+        socket.broadcast.emit('numberOfUser has changed', numberOfConnectedUser);
     });
 
     socket.on('chat username', function (data) {
-        socket.username = data;
+        socket.username = striptags(data);
     });
 
     socket.on('chat message', function (msg) {
@@ -54,9 +42,24 @@ io.on('connection', function (socket) {
     });
 
     socket.on('chat connection', function(data){
-       socket.username = data;
-        socket.emit('connection success', data);
-        socket.broadcast.emit('user is connected', data + " vient de se connecter");
+        /*console.log('LOUUUUUUUUUU BOYYYYYYYYYYYY');
+        console.log("STRIPTAG" + striptags(data));*/
+        numberOfConnectedUser++;
+        socket.username = striptags(data);
+
+        // When someone is connected to the chat we send back the username of the new user and the number of connected user
+        var dataToSend =
+        {
+            "nameUser" : socket.username,
+            "numberOfUserConnected" : numberOfConnectedUser
+        };
+
+        socket.emit('connection success', dataToSend);
+
+        // A modifier
+        //socket.broadcast.emit('user is connected', data + " vient de se connecter");
+        console.log("Nombre d'user connecté : " + numberOfConnectedUser);
+        socket.broadcast.emit('numberOfUser has changed', numberOfConnectedUser);
     });
 });
 
